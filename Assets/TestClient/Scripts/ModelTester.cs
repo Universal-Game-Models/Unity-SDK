18e4d76694b877b916468bfd55673137f639be69
CurtisNaples <curtis_fanning@hotmail.com>
2023-05-18 23:30:09 -0700

    add a basic webGL test client with a single scene that loads an asset from the url param

---
+++ Assets/TestClient/Scripts/ModelTester.cs
@@ -0,0 +1,76 @@
+using NaughtyAttributes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ModelTester : MonoBehaviour
+{
+    [SerializeField]
+    private string defaultAvatarId;
+    [SerializeField]
+    private UGADownloader defaultLoader;
+    [SerializeField]
+    private AvatarLoader avatarLoader;
+    [SerializeField]
+    private HumanoidToolLoader toolLoader;
+
+    [Button]
+    public void Test()
+    {
+        Load("8");
+    }
+    // Start is called before the first frame update
+    async void Start()
+    {
+        //Get the url params
+        var args = URLParameters.GetArguments();
+        if (args.ContainsKey("id"))
+        {
+            var nftId = args["id"];
+            await Load(nftId);
+        }
+    }
+
+    private async Task Load(string nftId)
+    {
+        //Get the nft metadata
+        var metadata = await UGADownloader.DownloadMetadataAsync(UGAAssetManager.METADATA_URI + nftId.PadLeft(64, '0') + ".json");
+        //Use the appropriate UGA Downloader to create it
+        var characterAttribute = Array.Find(metadata.attributes, a => a.trait_type == "Character");
+        if (characterAttribute != null)
+        {
+            if ((string)characterAttribute.value == "Avatar")
+            {
+                //Load the avatar
+                avatarLoader.Load(nftId);
+            }
+            else if ((string)characterAttribute.value == "Equipment")
+            {
+                //Create a default avatar
+                avatarLoader.onModelSuccess.AddListener((model) =>
+                {
+                    //When the avatar is loaded
+                    toolLoader.Load(nftId);
+                });
+                //Start loading the avatar and equipment
+                avatarLoader.Load(defaultAvatarId);
+            }
+        }
+        else
+        {
+            //Load an avatar to explore with
+            avatarLoader.Load(defaultAvatarId);
+            //Default load method
+            var structureAttribute = Array.Find(metadata.attributes, a => a.trait_type == "Structure");
+            if(structureAttribute != null && (string)structureAttribute.value == "Building")
+            {
+                defaultLoader.addBoxColliders = false;
+                //Buildings should always use mesh colliders for interior wall collisions
+                defaultLoader.addMeshColliders = true;
+            }
+            defaultLoader.Load(nftId);
+        }
+    }
+}
