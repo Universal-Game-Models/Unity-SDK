a7845703b11c07da9d2076f9485f127056ff3946
CurtisNaples <curtis_fanning@hotmail.com>
2023-06-07 22:26:36 -0700

    remove unused usings

--- Assets/UGM/Scripts/Examples/Inventory/Inventory.cs
+++ Assets/UGM/Scripts/Examples/Inventory/Inventory.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using static UGMManager;
 using static UGMDataTypes;
 using UnityEngine.EventSystems;
 
--- Assets/UGM/Scripts/Examples/Inventory/InventoryItems/Controls/InstantiatableInventoryControl.cs
+++ Assets/UGM/Scripts/Examples/Inventory/InventoryItems/Controls/InstantiatableInventoryControl.cs
@@ -1,7 +1,5 @@
 using Newtonsoft.Json;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
