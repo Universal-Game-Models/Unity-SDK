6522d3013d9b5ad044f6e9fe3a32f35efad26be8
Rolando <takaiyama00@gmail.com>
2023-06-20 18:17:45 -0700

    created new script for inventory item for skin swap

---
+++ Assets/UGM/Scripts/Examples/Features/SkinSwap/Core/SkinSwapInventoryItem.cs
@@ -0,0 +1,13 @@
+using UGM.Examples.Inventory.InventoryItems;
+using UnityEngine;
+
+namespace UGM.Examples.Features.SkinSwap.Core
+{
+    public class SkinSwapInventoryItem : HandEquipmentInventoryItem
+    {
+        protected override void Update()
+        {
+            
+        }
+    }
+}
