7d16c2049dfb11ef0771f5862fd4eccd83d559c1
Rolando <takaiyama00@gmail.com>
2023-06-29 16:54:09 -0700

    fixing missing prefab

--- CHANGELOG.md
+++
@@ -1,1 +0,0 @@
-version 0.0.1 Initial work. UGM CORE, Example scripts, Test Scene, and Skin Swap Scene
--- Samples~/UGMSamples/Scripts/Examples/ExampleUIEvents.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine.Events;
-
-public abstract class ExampleUIEvents
-{
-    public static UnityEvent<bool> OnShowCursor = new UnityEvent<bool>();
-}
